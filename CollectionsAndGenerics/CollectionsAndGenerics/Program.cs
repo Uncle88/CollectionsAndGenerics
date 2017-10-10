a3c663e5970fb6c03946e8171965b5667bd6266e
Uncle88 <andreyproskura88@gmail.com>
2017-10-10 18:03:46 +0300

    generic in C# 1.0

--- CollectionsAndGenerics/CollectionsAndGenerics/Program.cs
+++ CollectionsAndGenerics/CollectionsAndGenerics/Program.cs
@@ -6,7 +6,44 @@ namespace CollectionsAndGenerics
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GenericType1 gt1 = new GenericType1();
+            gt1.Add(10);
+            gt1.Add("value");
+            gt1.Add(2.5);
+
+            var val = gt1[1];
+            Console.WriteLine(val);
+        }
+    }
+
+    public class GenericType1
+    {
+        object[] elements;
+        int count;
+
+        public GenericType1()
+        {
+            elements = new object[5];
+        }
+
+        public object this[int index]
+        {
+            get{ return elements[index];}
+            set { elements[index] = value; }
+        }
+
+        public void Add(object parametr)
+        {
+            if (count == elements.Length)
+            {
+                object[] tmpArray = null;
+				elements.CopyTo(tmpArray, 0);
+				elements = new object[count * 2];
+				elements = tmpArray;
+			}
+
+            elements[count] = parametr;
+            count = count + 1;
         }
     }
 }
