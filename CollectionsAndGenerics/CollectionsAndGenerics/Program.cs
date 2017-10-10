60c82d9c076175347b7e5d8afda05bad40994ec9
Uncle88 <andreyproskura88@gmail.com>
2017-10-10 18:43:46 +0300

    Generic s in C# 2.0 and higher

--- CollectionsAndGenerics/CollectionsAndGenerics/Program.cs
+++ CollectionsAndGenerics/CollectionsAndGenerics/Program.cs
@@ -6,14 +6,26 @@ namespace CollectionsAndGenerics
     {
         public static void Main(string[] args)
         {
-            GenericType1 gt1 = new GenericType1();
-            gt1.Add(10);
-            gt1.Add("value");
-            gt1.Add(2.5);
+            Console.WriteLine("C# 1.0");
+            GenericType1 list1 = new GenericType1();
+            list1.Add(10);                                            //boxed
+			list1.Add("value");
+            list1.Add(2.5);                                           //boxed
 
-            var val = gt1[1];
-            Console.WriteLine(val);
-        }
+			var val = list1[1];                                       //No Casting Required
+			Console.WriteLine(val);
+
+			Console.WriteLine("\nC# 2.0");
+            GenericType2<double> list2 = new GenericType2<double>();
+
+            list2.Add(1);
+            list2.Add(2);
+            list2.Add(3);
+            // list2.Add("four");                                       //Compile Error
+
+            int valGeneric = (int)list2[0];                             //Casting Required
+			Console.WriteLine(valGeneric);
+		}
     }
 
     public class GenericType1
@@ -46,4 +58,35 @@ namespace CollectionsAndGenerics
             count = count + 1;
         }
     }
+
+    public class GenericType2<T>
+    {
+        private T[] elements;
+        private int count;
+
+        public GenericType2()
+        {
+            elements = new T[5];
+        }
+
+		public T this[int index]
+		{
+			get { return elements[index]; }
+			set { elements[index] = value; }
+		}
+
+        public void Add( T prm)
+        {
+            if (count == elements.Length)
+            {
+                T[] tmpArr = null;
+                elements.CopyTo(tmpArr, 0);
+                elements = new T[count * 2];
+                elements = tmpArr;
+            }
+
+            elements[count] = prm;
+            count = count + 1;
+        }
+	}
 }
